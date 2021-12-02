191c3420bfec7dd95fe3501fc266150a56512293
Nicolas <nperedoesquivelgmail.com>
2021-12-02 18:12:59 -0300

    Initializing the project and installing dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
+import { firestore, loginWithGoogle, auth, logOut } from './Firebase';
+import hearth from './icons/hearth.svg'
+
 
 function App() {
+  const [tweets, setTweets] = useState([]);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const cancelSubs = firestore.collection("tweets")
+      .onSnapshot((snapshot) => {
+        const tweets = snapshot.docs.map((doc) => {
+          return {
+            likes: doc.data().likes,
+            id: doc.id
+          };
+        });
+        setTweets(tweets);
+      });
+
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+      console.log(user);
+    })
+    return () => cancelSubs;
+  }, [])
+
+  const likeTweetHandler = (id, numLikes) => {
+    if (!numLikes) {
+      numLikes = 0;
+    }
+    firestore.doc(`tweets/${id}`).update({ likes: numLikes + 1 });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user ? (
+        <>
+          <div className="user-profile">
+            <img src={user.photoURL} alt="img" className="user-profile__pic" />
+            <p>¡Hola {user.displayName}!</p>
+            <button className="user-profile__logout" onClick={logOut}>Log out</button>
+          </div>
+        </>
+      ) : (
+          <button className="login-btn" onClick={loginWithGoogle}>
+            Login with google
+          </button>
+        )}
+      <TweetCard tweets={tweets} likeTweetHandler={likeTweetHandler} />
     </div>
   );
+}
+
+function TweetCard({ tweets, likeTweetHandler }) {
+  return (
+    tweets.map((tweet, i) =>
+      <div className="tweet__card">
+        <div className="icons">
+          <span>{tweet.likes ? tweet.likes : "0"}</span><span className="like__span" onClick={() => likeTweetHandler(tweet.id, tweet.likes)}><img height="13px" alt="hearth" src={hearth}></img></span>
+        </div>
+      </div>
+    ));
+
 }
 
 export default App;
---
+++ src/Firebase.js
@@ -0,0 +1,29 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDcTWQ0Fo-tflVgiCMtITa3kBfnaJLK90A",
+    authDomain: "devsunitedsp4.firebaseapp.com",
+    projectId: "devsunitedsp4",
+    storageBucket: "devsunitedsp4.appspot.com",
+    messagingSenderId: "168270308549",
+    appId: "1:168270308549:web:66c6606ad3d88d209abcac"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+// Exporta la funcionalidad de la DB
+export const firestore = firebase.firestore()
+// exporta el paquete de firebase para poder usarlo
+
+//exporta y crea un modulo de auth.
+export const auth = firebase.auth();
+//creando un nuevo provider.
+export const provider = new firebase.auth.GoogleAuthProvider();
+
+export const loginWithGoogle = () => auth.signInWithPopup(provider);
+export const logOut = () => auth.signOut();
+
+export default firebase;
